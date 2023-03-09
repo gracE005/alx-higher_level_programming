d3431b38cc68df8415a64287d7906329d6ad1b9c
gracE005 <gracenanaamamartin@gmail.com>
2023-03-09 14:45:00 -0800

    0x01.pythontask

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,44 +7,44 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new, *tmp;
-	int i, j;
+listint_t *new, *tmp;
+int i, j;
 
-	new = malloc(sizeof(listint_t));
-	if (!new)
-		return (NULL);
-	new->n = number, tmp = *head;
-	if (!(*head))
-		return (*head = new, new->next = NULL, new);
-	for (i = 0; tmp; i++)
-	{
-		if (number > tmp->n)
-		{
-			if (!tmp->next)
-				return (tmp->next = new, new->next = NULL, new);
-			tmp = tmp->next;
-			continue;
-		}
-		else
-		{
-			new->next = tmp;
-			if (tmp == *head)
-			{
-				*head = new;
-				break;
-			}
-			tmp = *head;
-			for (j = 0; j < i; j++)
-			{
-				if (j == (i - 1))
-				{
-					tmp->next = new;
-					break;
-				}
-				tmp = tmp->next;
-			}
-			break;
-		}
-	}
-	return (new);
+new = malloc(sizeof(listint_t));
+if (!new)
+return (NULL);
+new->n = number, tmp = *head;
+if (!(*head))
+return (*head = new, new->next = NULL, new);
+for (i = 0; tmp; i++)
+{
+if (number > tmp->n)
+{
+if (!tmp->next)
+return (tmp->next = new, new->next = NULL, new);
+tmp = tmp->next;
+continue;
+}
+else
+{
+new->next = tmp;
+if (tmp == *head)
+{
+*head = new;
+break;
+}
+tmp = *head;
+for (j = 0; j < i; j++)
+{
+if (j == (i - 1))
+{
+tmp->next = new;
+break;
+}
+tmp = tmp->next;
+}
+break;
+}
+}
+return (new);
 }
