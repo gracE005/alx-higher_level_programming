13b250cba619a799e19f64cff36e91502008e633
gracE005 <gracenanaamamartin@gmail.com>
2023-06-03 14:42:55 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,30 +3,32 @@
 A module that contains a single class
 """
 
+
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self._width = self._validate_dimension(width, "width")
-        self._height = self._validate_dimension(height, "height")
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
-        self._width = self._validate_dimension(value, "width")
+        if not isinstance(value, (int, float)):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
-        self._height = self._validate_dimension(value, "height")
-
-    def _validate_dimension(self, value, dimension_name):
         if not isinstance(value, (int, float)):
-            raise TypeError(f"{dimension_name} must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError(f"{dimension_name} must be >= 0")
-        return value
+            raise ValueError("height must be >= 0")
+        self.__height = value
