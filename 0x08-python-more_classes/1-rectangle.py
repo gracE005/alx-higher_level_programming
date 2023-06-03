3658c30abc2c3c515eda82167f7505ec33ac069f
gracE005 <gracenanaamamartin@gmail.com>
2023-06-03 15:02:37 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,32 +1,47 @@
 #!/usr/bin/python3
 """
-A module that contains a single class
+    A module that contains a single class
 """
 
 
 class Rectangle:
+    """Determines a reactangle"""
+
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        """Initializing of instance data"""
+        if not isinstance(width, (int, float)):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, (int, float)):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
+        """Recovers the value of `width`"""
         return self.__width
+
+    @property
+    def height(self):
+        """Reclaims the value of `height`"""
+        return self.__height
 
     @width.setter
     def width(self, value):
+        """Sets the value of atribute `width` to new value"""
         if not isinstance(value, (int, float)):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    @property
-    def height(self):
-        return self.__height
-
     @height.setter
     def height(self, value):
+        """Sets the value of atribute `height` to a new value"""
         if not isinstance(value, (int, float)):
             raise TypeError("height must be an integer")
         if value < 0:
