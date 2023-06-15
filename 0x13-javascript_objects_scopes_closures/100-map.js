5e8569719c416acdd20806e5ac2312535226baf7
gracE005 <gracenanaamamartin@gmail.com>
2023-06-15 16:43:13 -0700

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,18 @@
-
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+// Importing the list from 100-data.js
+const importList = require('./100-data.js').list;
+const newList = importList.map((Item, ItemIndex) => Item * ItemIndex);
+console.log(importList);
+console.log(newList);
+
+/*
+const dan = importList.map(x => x * indexOf(x));
+console.log(dan);
+const list = [1, 2, 3, 4, 5];
+list.map((x, y) => console.log(`The index of ${x} is ${y}`));
+const newList = importList.map((x, index) => {
+  console.log(index);
+  return x * index;
+});
+console.log(newList);
+*/
