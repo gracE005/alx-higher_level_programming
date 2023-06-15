c86086b3ad1ce3b9ca704ccc1c2754fa023b96b9
gracE005 <gracenanaamamartin@gmail.com>
2023-06-15 13:35:54 -0700

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-A module that contains a command that prints a text with two new lines after each of these characters: ., ? and :  
+This module defines `text_indentation`
+The function prints a text with 2 new lines after each of
+these characters: ., ? and :
 """
 
 
