61907d165def38a903e547e1ee99066481ec106a
gracE005 <gracenanaamamartin@gmail.com>
2023-06-15 13:39:19 -0700

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,40 +1,26 @@
 #!/usr/bin/python3
 """
-This module defines `text_indentation`
-The function prints a text with 2 new lines after each of
-these characters: ., ? and :
+    Contains a single function that handles text indenting
 """
 
 
 def text_indentation(text):
-    """splits a text into lines along "?", ":", "." followed by 2 new lines
-    Args:
-        text: input string
-    Returns:
-        No return
-    Raises:
-        TypeError: If text is not a string
     """
-
-    if type(text) is not str:
+        prints a text with 2 new lines after each
+        of these characters: ., ? and :
+    """
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-    flag = 0
-    a = 0
-    while a < len(text):
-        if flag == 0:
-            if text[a] == ' ':
-                a += 1
-                continue
-            else:
-                flag = 1
-        if flag == 1:
-            if text[a] == '?' or text[a] == '.' or text[a] == ':':
-                print(text[a])
-                print()
-                flag = 0
-            else:
-                if text[a + 1] == '\n':
-                     a += 1
-                     continue 
-                print(text[a], end="")
-           a += 1
+
+    begin = 0
+    delimeters = [".", "?", ":"]
+    for c in text:
+        if c.isspace() and begin == 0:
+            continue
+        elif c in delimeters:
+            print("{}\n\n".format(c), end="")
+            begin = 0
+            continue
+        else:
+            print("{}".format(c), end="")
+            begin += 1
